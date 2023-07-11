2b6df58fb68b0e041195c9185463991c82963e4d
luanpoppe <118562068+luanpoppe@users.noreply.github.com>
2023-07-11 12:00:19 -0300

    novo commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+function multiplicacao(x: number, y: number): number {
+  return x * y;
+}
+
+let testeMultiplicacao = multiplicacao(5, 3);
+console.log(testeMultiplicacao);
+
+function saudacao(nome: string): string {
+  return `Olá ${nome}`;
+}
+
+let testeSaudacao = saudacao("Maria");
+console.log(testeSaudacao);
